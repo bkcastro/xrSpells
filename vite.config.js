f3bcc86991e95fb072619368821bf02ef51ecce7
Brandon Castro <C1brandon626@gmail.com>
2024-07-12 18:36:21 -0700

    changed to http

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ export default defineConfig({
     server: {
         host: true,
         port: 3000,
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')),
-        },
     },
 });
