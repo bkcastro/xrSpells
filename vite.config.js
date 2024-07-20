1ffefac47528dc28820a511a2ea32aff44ba14dd
Brandon Castro <C1brandon626@gmail.com>
2024-07-20 09:34:20 -0700

    fixed https error

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
     server: {
         host: true,
         port: 3000,
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
-        }
+        // https: {
+        //     key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+        //     cert: fs.readFileSync(path.resolve(__dirname, 'server.crt'))
+        // }
     },
 });
